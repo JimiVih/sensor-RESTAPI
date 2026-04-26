a3c9a82a9ec94f6564c27fbc78830eb291c87534
Jimi <jimi.vihinen@proton.me>
2026-04-26 20:11:48 +0300

    add: descriptions

--- routers/block_router.py
+++ routers/block_router.py
@@ -6,14 +6,14 @@ from ..db.database import get_session
 
 router = APIRouter(prefix="/blocks", tags=["blocks"])
 
-@router.get("/list-sensors/from-blockID={blockID}", response_model=list[SensorDB], description="")
+@router.get("/list-sensors/from-blockID={blockID}", response_model=list[SensorDB], description="GETs list of all sensors in a specific Block")
 def list_sensors(blockID: str, session: Session = Depends(get_session)):
     return crud.list_sensors(session, blockID)
 
-@router.post("/add-block/sensorID={sensorID}&blockID={blockID}", response_model=BlockBase, description="")
+@router.post("/add-block/sensorID={sensorID}&blockID={blockID}", response_model=BlockBase, description="Creates a new Block with manually created unique identification (or name), and adds manually given sensor with sensorID")
 def add_block(sensorID: str, blockID: str, session: Session = Depends(get_session)):
     return crud.add_block(session, sensorID, blockID)
 
-@router.post("/add-sensor/sensorID={sensorID}&blockID={blockID}", response_model=BlockBase, description="")
+@router.post("/add-sensor/sensorID={sensorID}&blockID={blockID}", response_model=BlockBase, description="Adds sensor to block")
 def add_sensor(sensorID: str, blockID: str, session: Session = Depends(get_session)):
     return crud.add_sensor(session, sensorID, blockID)
--- routers/sensor_router.py
+++ routers/sensor_router.py
@@ -16,18 +16,18 @@ def list_info(sensorID: str, session: Session = Depends(get_session)):
 def list_all(session: Session = Depends(get_session)):
     return crud.list_all(session)
 
-@router.get("/list-by-status/where-status={curStatus}", response_model=list[SensorDB])
+@router.get("/list-by-status/where-status={curStatus}", response_model=list[SensorDB], description="lists all sensors with true or false status")
 def list_by_status(session: Session = Depends(get_session), curStatus: bool | None = False):
     return crud.list_status(session, curStatus)
 
-@router.post("", response_model=SensorDB, status_code=status.HTTP_201_CREATED)
+@router.post("", response_model=SensorDB, status_code=status.HTTP_201_CREATED, description="Creates a new sensor and adds it to the SensorDB")
 def add_sensor(newSensor: SensorCreate, session: Session = Depends(get_session)):
     return crud.add_sensor(session, newSensor)
 
-@router.patch("/sensor-status/sensor={sensorID}/set-status={setStatus}", response_model=bool, status_code=status.HTTP_202_ACCEPTED)
+@router.patch("/sensor-status/sensor={sensorID}/set-status={setStatus}", response_model=bool, status_code=status.HTTP_202_ACCEPTED, description="Sets a given status, true or false, to a sensor")
 def change_status(setStatus: bool, sensorID: str, session: Session = Depends(get_session)):
     return crud.change_status(session, sensorID, setStatus)
 
-@router.delete("/{sensorID}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{sensorID}", status_code=status.HTTP_204_NO_CONTENT, description="Deletes selected sensor from the SensorDB")
 def remove_sensor(sensorID: str, session: Session = Depends(get_session)):
     return crud.remove_sensor(session, sensorID)
