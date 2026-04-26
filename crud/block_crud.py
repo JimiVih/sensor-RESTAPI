30a7a23b3adc444e7477c1fedeaba5450e7f8548
Jimi <jimi.vihinen@proton.me>
2026-04-26 04:12:16 +0300

    Feat: POST new block
    Can POST new block

--- crud/block_crud.py
+++ crud/block_crud.py
@@ -1,7 +1,52 @@
 #LOHKO CRUDE
+from fastapi import HTTPException
+from sqlmodel import Session, select
+from ..db.models import BlockDB, BlockBase, SensorDB
+
 
 #Luo lohko()
+def add_block(session: Session, sensorID: str, blockID: str):
+    if sensorID == None:
+        raise HTTPException(status_code=406, detail="sensorID CANNOT be empty! Please insert a valid sensorID.")
+    if not session.exec(select(SensorDB).where(SensorDB.sensorID==sensorID)).first():
+        raise HTTPException(status_code=404, detail=f"No sensor with ID: '{sensorID}' was found in the database")
+    if session.exec(select(BlockDB)).all().__len__() == 0:
+        #_sID = session.exec(select(SensorDB).where(SensorDB.sensorID==sensorID)).one().id
+        newBlock = BlockBase
+        newBlock.blockID = blockID
+        newBlock.sensorID = sensorID
+        newBlock.id = None
+        _b = BlockBase.model_validate(newBlock)
+        _bDB = BlockDB.model_validate(_b)
+        session.add(_b)
+        session.commit()
+        session.refresh(_b)
 
+        session.add(_bDB)
+        session.commit()
+        session.refresh(_bDB)
+
+        return _b
+    else:
+        if session.exec(select(BlockDB).where(BlockDB.blockID==blockID)).first():
+            raise HTTPException(status_code=406, detail=f"Block with blockID: '{blockID}' already exist in the database! Please use free blockID.")
+        else:
+            newBlock = BlockBase
+            newBlock.blockID = blockID
+            newBlock.sensorID = sensorID
+            newBlock.id = None
+            _b = BlockBase.model_validate(newBlock)
+            _bDB = BlockDB.model_validate(_b)
+            session.add(_b)
+            session.commit()
+            session.refresh(_b)
+
+            session.add(_bDB)
+            session.commit()
+            session.refresh(_bDB)
+
+            return _b
+        
 #Lisää anturi lohkoon()
 
 #Listaa lohkon anturit()
---
+++ routers/block_router.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Depends, status, APIRouter
+from sqlmodel import Session
+from ..db.models import BlockDB, BlockBase
+from ..crud import block_crud as crud
+from ..db.database import get_session
+
+router = APIRouter(prefix="/blocks", tags=["blocks"])
+
+@router.post("/add-block/sensorID={sensorID}&blockID={blockID}", response_model=BlockBase, description="")
+def add_block(sensorID: str, blockID: str, session: Session = Depends(get_session)):
+    return crud.add_block(session, sensorID, blockID)
