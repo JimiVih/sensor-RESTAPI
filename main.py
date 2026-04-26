ca4b2b73e5aaca3fcdb256c661af312a910f344c
Jimi <jimi.vihinen@proton.me>
2026-04-26 04:15:15 +0300

    Add: include block_router

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from sqlmodel import SQLModel
-from .routers import sensor_router
+from .routers import sensor_router, block_router
 from .db.database import create_db_and_engine
 from contextlib import asynccontextmanager
 
@@ -12,6 +12,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(sensor_router.router)
+app.include_router(block_router.router)
 # @app.get("/test")
 # async def testi():
 #     return "moro"
